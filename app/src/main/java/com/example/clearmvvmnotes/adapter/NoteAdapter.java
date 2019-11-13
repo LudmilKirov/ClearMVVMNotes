7be6bde2bb2f322c9d1caadc3e32681925b6c909
LudmilKirov <ludmil9595@gmail.com>
2019-11-13 18:14:41 +0200

    Add proper adapter

--- app/src/main/java/com/example/clearmvvmnotes/adapter/NoteAdapter.java
+++ app/src/main/java/com/example/clearmvvmnotes/adapter/NoteAdapter.java
@@ -1,4 +1,98 @@
 package com.example.clearmvvmnotes.adapter;
 
-public class NoteAdapter {
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.clearmvvmnotes.R;
+import com.example.clearmvvmnotes.model.Note;
+
+import java.util.ArrayList;
+
+public class NoteAdapter extends ListAdapter<Note,NoteAdapter.NoteHolder> {
+    private static final String TAG = "NoteAdapter";
+    private OnNoteListener mOnNoteListener;
+    private ArrayList<Note> mNotes = new ArrayList<>();
+
+    public NoteAdapter() {
+        super(DIFF_CALLBACK);
+    }
+
+    private static final DiffUtil.ItemCallback<Note> DIFF_CALLBACK = new DiffUtil.ItemCallback<Note>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getId() == newItem.getId();
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getTitle().equals(newItem.getTitle())
+                    && oldItem.getDescription().equals(newItem.getDescription())
+                    &&oldItem.getPriority()==newItem.getPriority();
+        }
+    };
+
+    //Create and return NoteHolder
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item,parent,false);
+        return new NoteHolder(itemView);
+    }
+
+    //Give the proper attributes
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note currentNote = getItem(position);
+        holder.textViewTile.setText(currentNote.getTitle());
+        holder.textViewDescription.setText(currentNote.getDescription());
+        holder.getTextViewPriority.setText(String.valueOf(currentNote.getPriority()));
+    }
+
+    public Note getNoteAt(int position){
+        return getItem(position);
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView textViewTile;
+        private TextView textViewDescription;
+        private TextView getTextViewPriority;
+
+        OnNoteListener mOnNoteListener;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTile= itemView.findViewById(R.id.text_view_title);
+            getTextViewPriority=itemView.findViewById(R.id.text_view_priority);
+            textViewDescription=itemView.findViewById(R.id.text_view_description);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    if(mOnNoteListener != null &&  position != RecyclerView.NO_POSITION) {
+                        mOnNoteListener.onNoteClick(position);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onClick(View view) {
+            Log.d(TAG, "onClick: " + getAdapterPosition());
+            mOnNoteListener.onNoteClick(getAdapterPosition());
+        }
+    }
+
+    public interface OnNoteListener{
+        void onNoteClick(int position);
+    }
 }
--- app/src/main/java/com/example/clearmvvmnotes/view/MainActivity.java
+++ app/src/main/java/com/example/clearmvvmnotes/view/MainActivity.java
@@ -1,16 +1,120 @@
 package com.example.clearmvvmnotes.view;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.clearmvvmnotes.R;
+import com.example.clearmvvmnotes.adapter.NoteAdapter;
+import com.example.clearmvvmnotes.model.Note;
+import com.example.clearmvvmnotes.viewmodel.NoteViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
+    public static final int ADD_NOTE_REQUEST = 1;
+    public static final int EDIT_NOTE_REQUEST = 2;
+    private RecyclerView recyclerView;
+    private AlertDialog mAlertDialog;
+    private NoteAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initRecyclerView();
+        setTheAdapter();
+        observeTheList();
+        observeForAlertDialog();
+        swipeToDelete();
+
     }
+
+
+    private void swipeToDelete(){
+        //To swipe to delete
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                //get the object that wnat to be delete
+                noteViewModel.delete(adapter.getNoteAt(viewHolder.getAdapterPosition()));
+                //Toast.makeText(MainActivity.this, "Note deleted", Toast.LENGTH_SHORT).show();
+            }
+        }).attachToRecyclerView(recyclerView);
+
+        //Initialize the floating button that add a new note
+        FloatingActionButton buttonAddNote = findViewById(R.id.floatingActionButton);
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddEditNoteActivity.class);
+                startActivityForResult(intent, ADD_NOTE_REQUEST);
+            }
+        });
+    }
+
+    private void initRecyclerView(){
+        //Initialize the recyclerview
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+    }
+
+    private void setTheAdapter(){
+        //Set adapter for the recyclerview
+        adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void observeTheList(){
+        //To observe the view model for a change of the list
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                adapter.submitList(notes);
+            }
+        });
+    }
+
+    private void observeForAlertDialog(){
+        //Create the dialog for the loading
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Loading");
+        builder.setCancelable(false);
+        mAlertDialog = builder.create();
+        //Create the observer for the live data that changes in the async task for add method.
+        noteViewModel.getMutableLiveData().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if (!aBoolean) {
+                    mAlertDialog.show();
+                } else {
+                    mAlertDialog.hide();
+                }
+            }
+        });
+    }
+
+
+
 }
